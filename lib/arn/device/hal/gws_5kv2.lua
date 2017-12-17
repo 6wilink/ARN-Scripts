3c50a5163947a0e6a339a67caed2f61e9e88243e
6WiLink <6wilink@users.noreply.github.com>
2017-12-17 11:06:32 +0800

    Compatible & call "rfinfo/set*" (#11)
    * Compatible with "rfinfo"
    * Add support of "arn -M adhoc"
    * Fix wrong channel number( replace "grep" with "awk")
    * Remove details from UserApp, save to "ARN-Scripts"

--- lib/arn/device/hal/gws_5kv2.lua
+++ lib/arn/device/hal/gws_5kv2.lua
@@ -1,5 +1,5 @@
 --[[
-Note: 
+Note:
     Although GWS5Kv1 & GWS5Kv2 share the same methods and functions,
     this copy will let maintainer handle 2 types of hardware.
 
@@ -38,17 +38,17 @@ gws_radio.cmd.rfinfo_wait   = 'sleep 1'
 gws_radio.cmd.rfinfo        = 'echo > /tmp/.GWS5Kv2.tmp; `which gws5001app` rfinfo >/dev/null 2>&1; `which gws5001app` rfinfo 2>/dev/null > /tmp/.GWS5Kv2.tmp'
 gws_radio.cmd.rfinfo_all    = 'cat /tmp/.GWS5Kv2.tmp 2>/dev/null'
 gws_radio.cmd.region        = 'cat /tmp/.GWS5Kv2.tmp 2> /dev/null | grep Region | grep [01]* -o'
-gws_radio.cmd.channel       = 'cat /tmp/.GWS5Kv2.tmp 2> /dev/null | grep Chan: | grep [0-9]* -o'
+gws_radio.cmd.channel       = "cat /tmp/.GWS5Kv2.tmp 2> /dev/null | grep Chan: | awk '{print $2}'"
 gws_radio.cmd.txpower       = 'cat /tmp/.GWS5Kv2.tmp 2> /dev/null | grep Tx | grep Power | grep [0-9\.]* -o'
 gws_radio.cmd.chanbw        = 'cat /tmp/.GWS5Kv2.tmp 2> /dev/null | grep Chan | grep BW | grep [0-9]* -o'
 gws_radio.cmd.rxgain        = 'cat /tmp/.GWS5Kv2.tmp 2> /dev/null | grep Rx | grep Gain | grep [0-9\.]* -o'
 
-gws_radio.cmd.region_set    = '`which gws5001app` setregion %s 2> /dev/null '
-gws_radio.cmd.channel_set   = '`which gws5001app` setchan %s 2> /dev/null '
-gws_radio.cmd.txpower_set   = '`which gws5001app` settxpwr %s 2> /dev/null '
-gws_radio.cmd.chanbw_set    = '`which gws5001app` setchanbw %s 2> /dev/null '
-gws_radio.cmd.rxgain_set    = '`which gws5001app` setrxgain %s 2> /dev/null '
-gws_radio.cmd.txchain_set   = '`which gws5001app` rf%s 2> /dev/null '
+gws_radio.cmd.region_set    = '`which gws5001app` setregion %s 2> /dev/null; setregion %s 2> /dev/null; '
+gws_radio.cmd.channel_set   = '`which gws5001app` setchan %s 2> /dev/null; setchan %s 2> /dev/null; '
+gws_radio.cmd.txpower_set   = '`which gws5001app` settxpwr %s 2> /dev/null; settxpwr %s 2> /dev/null; '
+gws_radio.cmd.chanbw_set    = '`which gws5001app` setchanbw %s 2> /dev/null; setchanbw %s 2> /dev/null; '
+gws_radio.cmd.rxgain_set    = '`which gws5001app` setrxgain %s 2> /dev/null; setrxgain %s 2> /dev/null; '
+gws_radio.cmd.txchain_set   = '`which gws5001app` rf%s 2> /dev/null; rf%s 2> /dev/null; '
 
 function gws_radio.rfinfo_init()
     DBG(sfmt("GWS5Kv2----> update_init()"))
@@ -116,25 +116,25 @@ Tasks:
 function gws_radio.UPDATE_RT()
     DBG(sfmt("GWS5Kv2> update_rt (@%d)", os.time()))
     local result = {}
-    
+
     gws_radio.rfinfo_init()
-    
+
     result.region = gws_radio.update_item('region') or '0'
     DBG(sfmt("GWS5Kv2----> update_item() region=%s", result.region))
-    
+
     result.channo = gws_radio.update_item('channel') or '0'
     result.freq = uhf.channel_to_freq(result.region, result.channo)
     DBG(sfmt("GWS5Kv2----> update_item() channel=%s,freq=%s", result.channo, result.freq))
-    
+
     result.txpwr = gws_radio.update_item('txpower') or '0'
     DBG(sfmt("GWS5Kv2----> update_item() txpower=%s", result.txpwr))
-    
+
     result.chanbw = gws_radio.update_item('chanbw') or '0'
     DBG(sfmt("GWS5Kv2----> update_item() chanbw=%s", result.chanbw))
-    
+
     result.rxgain = gws_radio.update_item('rxgain') or '0'
     DBG(sfmt("GWS5Kv2----> update_item() rxgain=%s", result.rxgain))
-    
+
     --result.ts = os.time()
     --gws_radio.rfinfo_clean()
     return result
@@ -144,22 +144,22 @@ function gws_radio.SET_RT(key, value)
     local result = true
     DBG(sfmt("GWS5Kv2> set_rt k=%s,value=%s (@%d)", key or '-', value or '-', os.time()))
     if (key == 'region') then
-        exec(sfmt(gws_radio.cmd.region_set, value))
+        exec(sfmt(gws_radio.cmd.region_set, value, value))
         result = false
     elseif (key == 'channel' or key == 'channo') then
-        exec(sfmt(gws_radio.cmd.channel_set, value))
+        exec(sfmt(gws_radio.cmd.channel_set, value, value))
         result = false
     elseif (key == 'txpower' or key == 'txpwr') then
-        exec(sfmt(gws_radio.cmd.txpower_set, value))
+        exec(sfmt(gws_radio.cmd.txpower_set, value, value))
         result = false
     elseif (key == 'chanbw') then
-        exec(sfmt(gws_radio.cmd.chanbw_set, value))
+        exec(sfmt(gws_radio.cmd.chanbw_set, value, value))
         result = false
     elseif (key == 'rxgain') then
-        exec(sfmt(gws_radio.cmd.rxgain_set, value))
+        exec(sfmt(gws_radio.cmd.rxgain_set, value, value))
         result = false
     elseif (key == 'tx' or key == 'rf') then -- 'gws5001app rfon|rfoff'
-        exec(sfmt(gws_radio.cmd.txchain_set, value))
+        exec(sfmt(gws_radio.cmd.txchain_set, value, value))
         result = false
     end
     gws_radio.rfinfo_clean()
